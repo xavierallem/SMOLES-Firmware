d7b209aed6e4732824ff4167b8124eca94568ed3
Max Weißer <max_weisser@gmx.de>
2024-06-17 12:27:25 +0200

    Fix global variables

--- src/Constants.h
+++ src/Constants.h
@@ -9,7 +9,4 @@
 #define CHARACTERISTIC_UUID_RX  "4ac8a682-9736-4e5d-932b-e9b31405049c"
 #define CHARACTERISTIC_UUID_TX  "0972EF8C-7613-4075-AD52-756F33D4DA91"
 
-// Global parameter to check if bluetooth device is connected
-// bool bluetooth_device_connected = false;
-
 #endif
---
+++ src/Globals.h
@@ -0,0 +1,6 @@
+#ifndef SMOLES_GLOBALS_H
+#define SMOLES_GLOBALS_H
+
+extern bool bluetooth_device_connected;
+
+#endif
